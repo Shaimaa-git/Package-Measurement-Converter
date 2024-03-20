e947fcd6b59636a3fe6f70f82e36969a533c2801
shaimaa-git <shaimaasaidmo@gmail.com>
2024-03-20 12:22:42 +0400

    Use scanner to allow user to enter input

---
+++ PakcageMeasurementConverter.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class PackageMeasurementConverter {
+    
+        public static void main(String[] args) {
+            //create a scanner class to take the input from user
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the input string: ");
+            String input = scanner.nextLine();
+            scanner.close();
+    }
+}
